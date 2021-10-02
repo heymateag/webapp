64fc9152d344076e0e9169b1b79cb31ca41c65f8
ehsan <ehsan.ghasemi.se7en@gmail.com>
2021-10-02 14:03:54 +0330

    noti

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,6 @@ module.exports = {
   moduleNameMapper: {
     '\\.(css|scss|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|tgs)$':
       '<rootDir>/tests/staticFileMock.js',
-    '^teact/(.*)': '<rootDir>/src/lib/teact/$1',
-    'teact-dom': '<rootDir>/src/lib/teact/teact-dom.ts',
   },
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/',
