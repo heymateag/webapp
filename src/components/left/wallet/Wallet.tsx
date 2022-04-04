c4a03ae3b915277b0356764a2af1c22a02c551a4
alimoosavi <alimusavi67@gmail.com>
2022-04-04 13:45:55 +0430

    fix reopen qrcode scan, clean code

--- src/components/left/wallet/Wallet.tsx
+++ src/components/left/wallet/Wallet.tsx
@@ -19,7 +19,6 @@ import { pick } from '../../../util/iteratees';
 import Select from '../../ui/Select';
 import { axiosService } from '../../../api/services/axiosService';
 import walletLoggerService from '../../common/helpers/walletLoggerService';
-
 import { useWalletConnectQrModal } from './hooks/useWalletConnectQrModal';
 
 export type OwnProps = {
@@ -40,122 +39,69 @@ const Wallet: FC <OwnProps & DispatchProps> = ({ onReset, showNotification }) =>
   const [kit, setKit] = useState<any>();
   const [width, setWidth] = useState<number>(0);
   const [isConnected, setIsConnected] = useState(false);
-  const [loadingBalance, setLoadingBalance] = useState(true);
+  const [loadingBalance, setLoadingBalance] = useState(false);
   const [uri, setUri] = useState<string>('');
   const [balanceType, setBalanceType] = useState('cUSD');
   const [transactionList, setTransactionList] = useState([]);
-
-  const provider = useMemo(() => {
-    return new WalletConnectProvider({
-      rpc: {
-        44787: 'https://alfajores-forno.celo-testnet.org',
-        42220: 'https://forno.celo.org',
-      },
-      bridge: 'https://wc-bridge.heymate.works/',
-      qrcode: false,
-      clientMeta: {
-        description: 'Just a test description !',
-        icons: [],
-        url: 'www.ehsan.com',
-        name: 'Heymate App',
-      },
-    });
-  }, []);
-
-  // const [wcProvider, setWcProvider] = useState<any>(provider);
-  const handleCLoseWCModal = () => {
-    setOpenModal(false);
-    provider.isConnecting = false;
-    setLoadingBalance(false);
-  };
-
-  useWalletConnectQrModal(uri, openModal, handleCLoseWCModal);
-
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
-
+  const [provider, setProvider] = useState<any>();
   const [balance, setBalance] = useState<IBalance>({
     cUSD: '0',
     CELO: '0',
     cEUR: '0',
     cREAL: '0',
   });
-  const lang = useLang();
-
-  const handleOpenWCModal = async () => {
-    if (uri === '') {
-      await provider.enable();
-    }
-    setOpenModal(true);
-    // renderUriAsQr();
-  };
-  /**
-   * Get Account Balance
-   */
-  provider.connector.on('display_uri', (err, payload) => {
+  const connect = async () => {
+    const providerItem = new WalletConnectProvider({
+      rpc: {
+        44787: 'https://alfajores-forno.celo-testnet.org',
+        42220: 'https://forno.celo.org',
+      },
+      bridge: 'https://wc-bridge.heymate.works/',
+    });
     walletLoggerService({
       description: 'start connecting wallet',
       status: 'Waiting',
     });
-    setIsConnected(false);
-    const wcUri = payload.params[0];
-
-    setUri(wcUri);
-    setOpenModal(true);
-  });
-
-  const makeKitsFromProvideAndGetBalance = useCallback(async (address?: string) => {
-    const reconnectTimeOut = setTimeout(() => {
-      if (loadingBalance) {
-        showNotification({ message: 'connection failed, please check your connection and retry' });
-        setLoadingBalance(false);
-        provider.disconnect();
-        // setWcProvider(provider);
-        window.location.reload();
-      }
-    }, 60000);
-    // @ts-ignore
-    const web3 = new Web3(provider);
-    // @ts-ignore
+    await providerItem.enable();
+    setLoadingBalance(true);
+    // showNotification({ message: 'Successfully Connected to Wallet !' });
+    const web3 = new Web3(providerItem);
     const myKit = newKitFromWeb3(web3);
-
-    const walletAddress = address || provider.accounts[0];
-
+    // eslint-disable-next-line prefer-destructuring
+    myKit.defaultAccount = providerItem.accounts[0];
     const accounts = await myKit.web3.eth.getAccounts();
     // eslint-disable-next-line prefer-destructuring
     myKit.defaultAccount = accounts[0];
     await myKit.setFeeCurrency(CeloContract.StableToken);
-    const accountBalance = await newKitBalances(myKit, walletAddress);
-    clearTimeout(reconnectTimeOut);
+    const accountBalance = await newKitBalances(myKit, accounts[0]);
     setBalance(accountBalance);
-
     setLoadingBalance(false);
-
     setKit(myKit);
-    // setWcProvider(provider);
-  }, [provider]);
-
-  provider.on('disconnect', (code: number, reason: string) => {
-    showNotification({ message: reason });
-    setIsConnected(false);
-    // setWcProvider(provider);
-  });
-
-  provider.onConnect(() => {
-    walletLoggerService({
-      description: 'connected wallet',
-      status: 'Success',
-    });
-    setOpenModal(false);
+    setProvider(providerItem);
     setIsConnected(true);
-    showNotification({ message: 'Successfully Connected to Wallet !' });
-    // setWcProvider(provider);
-    makeKitsFromProvideAndGetBalance();
-    WalletConnectQRCodeModal.close();
-  });
+    providerItem.on('accountsChanged', (act) => {
+      console.log(act);
+    });
+    providerItem.onConnect(() => {
+      walletLoggerService({
+        description: 'connected wallet',
+        status: 'Success',
+      });
+      setIsConnected(true);
+      showNotification({ message: 'Successfully Connected to Wallet !' });
+    });
+  };
 
-  const getTransactions = useCallback(async () => {
+  useEffect(() => {
+    setWidth(window.innerWidth);
+    connect();
+  }, []);
+
+
+  const lang = useLang();
+
+
+  const getTransactions = async () => {
     const baseURL = provider.chainId !== 44787
       ? 'https://explorer.celo.org/'
       : 'https://alfajores-blockscout.celo-testnet.org/';
@@ -168,28 +114,13 @@ const Wallet: FC <OwnProps & DispatchProps> = ({ onReset, showNotification }) =>
     const weiEther = kit?.web3?.utils?.toWei('1', 'ether');
     const filter = response.data.result.filter((row) => (row.value / weiEther) > 0.01);
     setTransactionList(filter);
-  }, [kit?.web3?.utils, provider.accounts, provider.chainId]);
-
-  useEffect(() => {
-    const reconnectToProvider = async () => {
-      await provider.enable()
-        .then((res) => {
-          setIsConnected(true);
-          makeKitsFromProvideAndGetBalance(res[0]);
-        });
-    };
-    if (provider.isWalletConnect) {
-      reconnectToProvider();
-    } else {
-      setLoadingBalance(false);
-    }
-  }, [makeKitsFromProvideAndGetBalance, provider]);
+  };
 
   useEffect(() => {
     if (kit) {
       getTransactions();
     }
-  }, [getTransactions, kit]);
+  }, [provider, kit]);
 
   const handleChangeCurrency = (e: any) => {
     const filter = e.target.value;
@@ -260,7 +191,7 @@ const Wallet: FC <OwnProps & DispatchProps> = ({ onReset, showNotification }) =>
           && (
             <div className="btn-row">
               <div id="cashout" className="btn-holder">
-                <Button onClick={handleOpenWCModal} isText size="smaller" color="primary">
+                <Button onClick={connect} isText size="smaller" color="primary">
                   <span>Connect</span>
                 </Button>
               </div>
